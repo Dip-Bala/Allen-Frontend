6262d7920c2b087a1dbe05750a1afcfca07d522e
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2025-04-14 02:53:24 +0530

    Navbar structure complete

--- src/containers/Navbar.jsx
+++ src/containers/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
 const style={
-  height: "20vh",
-
+  height: "100px",
+  backgroundColor: "white",
+  width: "100vw",
+  fontFamily:"sans-serif"
+}
+const upperStyle = {
+  display: "flex", 
+  justifyContent: "space-between", 
+  alignItems: "center", 
+  backgroundColor: "skyblue", 
+  padding: 20
 }
 export default function Navbar() {
   return (
     <div style = {style}>
-      <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-        <div>
+      <div style={upperStyle}>
+        <div style={{fontWeight: "bolder", fontSize: 20, color: "darkblue"}}>
           {/* <img src="" alt="" /> */}
           ALLEN
         </div>
@@ -25,12 +34,13 @@ export default function Navbar() {
           <div>Login</div>
         </div>
       </div>
-      <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <span>
+      <div style={{display: "flex", justifyContent: "center", alignItems: "center",  backgroundColor: "lightgreen", padding: 7, fontSize : 12, gap: 5}}>
+        <span style={{display: "flex", justifyContent: "center", alignItems: "center", color: "yellow", fontWeight: 500}}>
         <img src="https://res.cloudinary.com/dpzpn3dkw/image/upload/w_1600,f_avif,q_auto/v1742470106/mgdwn6mv7zfwxiopuntt.png?_upload_ref=ic_img_tool&__ar__=1.00" alt="clock"  style={{width:20}}/>
         BATCHES START 16 APR 
+        <img src="https://res.cloudinary.com/dpzpn3dkw/image/upload/w_400,f_auto,q_auto/v1733741024/lpbkzdbke54wl6cevwjo.svg?_upload_ref=ic_img_tool&__ar__=1.06" alt="clock"  style={{width:20}}/>
         </span>
-        <span>Win upto 90% AOSAT scholarship  <img src="https://res.cloudinary.com/dpzpn3dkw/image/upload/w_400,f_auto,q_auto/v1733741024/lpbkzdbke54wl6cevwjo.svg?_upload_ref=ic_img_tool&__ar__=1.06" alt="clock"  style={{width:20}}/>. Register for FREE</span>
+        <span style={{color: "white", fontWeight: 600}}>Win upto 90% AOSAT scholarship. Register for FREE</span>
       </div>
     </div>
   )
--- src/containers/Scholarship.jsx
+++ src/containers/Scholarship.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 
+const parentStyle = {
+  height: 250,
+  width: "100vw",
+  backgroundColor: "lightgray",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: 100,
+  paddingLeft : 200,
+  paddingRight : 200,
+  fontWeight:600
+
+  
+}
 export default function Scholarship() {
   return (
-    <div>Scholarship</div>
+    <div style={parentStyle}>
+      <div style={{width: "30%", display:"flex", flexDirection: "column", gap: 10}}>
+        <div style={{fontSize: 30}}>
+        Get up to 90% scholarship on ALLEN Courses
+        </div>
+        <div style={{display:"flex", flexDirection: "column", gap: 10}}>
+          <div style={{fontSize: 20}}>
+          Register for scholarship
+          </div>
+          <div style={{display:"flex", gap: 10}}>
+            <button style={{padding: 10, border: "2px solid blue", fontSize: 16, fontWeight: 500, borderRadius: 20}}>Online Classes</button>
+            <button style={{padding: 10, border: "2px solid blue", fontSize: 16, fontWeight: 500, borderRadius: 20}}>Offline Classes</button>
+          </div>
+        </div>
+      </div>
+      <div style={{width: "30%"}}>
+        <img src="https://res.cloudinary.com/dpzpn3dkw/image/upload/w_800,f_avif,q_auto/v1733911179/m6l4kbtrdldjtbbzuyrk.webp?_upload_ref=ic_img_tool&__ar__=1.78" style={{height: 200}}></img>
+      </div>
+    </div>
   )
 }
